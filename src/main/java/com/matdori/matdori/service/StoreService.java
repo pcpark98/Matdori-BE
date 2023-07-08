276d47920f7b9306b1be26963d0083b53083b434
StrangeMin <mim0107@nate.com>
2023-07-08 22:40:18 +0900

    feat : 가게 족보 불러오기 API 생성

--- src/main/java/com/matdori/matdori/service/StoreService.java
+++ src/main/java/com/matdori/matdori/service/StoreService.java
@@ -1,8 +1,10 @@
 package com.matdori.matdori.service;
 
 import com.matdori.matdori.domain.Category;
+import com.matdori.matdori.domain.Jokbo;
 import com.matdori.matdori.domain.Menu;
 import com.matdori.matdori.domain.Store;
+import com.matdori.matdori.repositoy.JokboRepository;
 import com.matdori.matdori.repositoy.StoreRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,9 +18,12 @@ import java.util.List;
 public class StoreService {
 
     private final StoreRepository storeRepository;
+    private final JokboRepository jokboRepository;
     public List<Store> findAll() { return storeRepository.findAll(); }
     public Store findOne(Long id) {return storeRepository.findOne(id); }
 
     public List<Category> findAllCategoryWithMenu (Long id) { return storeRepository.findAllCategoryWithMenu(id);}
+
+    public List<Jokbo> findAllJokbo(Long storeId) { return jokboRepository.findByStoreIndex(storeId);}
 
 }
