59ed8bdd2866db297189e7909dad12d0394d6bba
StrangeMin <mim0107@nate.com>
2023-07-10 16:33:15 +0900

    feat : 회원가입, 비밀번호 찾기 시 이메일 인증여부를 체크하는 로직 추가

--- src/main/java/com/matdori/matdori/service/AuthorizationService.java
+++ src/main/java/com/matdori/matdori/service/AuthorizationService.java
@@ -1,5 +1,6 @@
 package com.matdori.matdori.service;
 
+import com.matdori.matdori.domain.EmailAuthorizationType;
 import com.matdori.matdori.domain.User;
 import com.matdori.matdori.exception.*;
 import com.matdori.matdori.repositoy.UserRepository;
@@ -40,16 +41,33 @@ public class AuthorizationService {
             throw new InsufficientPrivilegesException(ErrorCode.INSUFFICIENT_PRIVILEGES);
     }
 
-    public static void logout(HttpSession session){
-        session.invalidate();
+    public static void logout(){
+        String sessionCookie = SessionUtil.getSessionCookie(); // session의 key값을 가져온다.
+        if(sessionCookie == null)  // 쿠키를 보내지 않은 경우
+            throw new ExpiredSessionException(ErrorCode.INVALID_REQUIRED_COOKIE);
+        else if(SessionUtil.getAttribute(sessionCookie) == null) // 세션이 만료된 경우
+            throw new ExpiredSessionException(ErrorCode.EXPIRED_SESSION);
+
+         SessionUtil.getSession().removeAttribute(sessionCookie);
     }
 
-    public static void checkAuthorizationNumber(String number){
+    public static void checkAuthorizationNumber(String number, EmailAuthorizationType type){
         if(number == null) // 인증번호를 보내지 않은 경우
             throw new InvalidRequiredParamException(ErrorCode.INVALID_REQUIRED_PARAM);
         else if(SessionUtil.getAttribute(number) == null) // 인증 가능 시간이 끝났거나 유효하지 않은 번호일 경우
             throw new ExpiredSessionException(ErrorCode.EXPIRED_SESSION);
 
-        SessionUtil.getSession().removeAttribute(number); // 인증완료 세션 삭제
+        // 이메일 인증 없이 회원가입, 비밀번호 변경 하는 것을 막기 위한 로직
+        // { email : type } 으로 세션 저장
+        String email = SessionUtil.getAttribute(number);
+        SessionUtil.setAttribute(email, type.name());
+        SessionUtil.getSession().removeAttribute(number);
+    }
+
+    public static void checkEmailVerificationCompletion(String email, EmailAuthorizationType type){
+        if (SessionUtil.getAttribute(email) == null || SessionUtil.getAttribute(email) != type.name())
+            throw new IncompleteEmailVerificationException(ErrorCode.INCOMPLETE_EMAIL_VERIFICATION);
+
+        SessionUtil.getSession().removeAttribute(email);
     }
 }
