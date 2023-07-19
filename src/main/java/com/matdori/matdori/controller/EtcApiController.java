006fa4a5b50e523bfbe49de459c16ac70bb2fba4
sp.arc <pcpark98@naver.com>
2023-07-19 21:37:24 +0900

    chore: 기타 API 스웨거 작업

--- src/main/java/com/matdori/matdori/controller/EtcApiController.java
+++ src/main/java/com/matdori/matdori/controller/EtcApiController.java
@@ -4,6 +4,9 @@ import com.matdori.matdori.domain.Notice;
 import com.matdori.matdori.domain.Response;
 import com.matdori.matdori.domain.TermsOfService;
 import com.matdori.matdori.service.EtcService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
@@ -16,6 +19,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Tag(name = "기타 API", description = "공지사항, 이용약관 등에 사용할 API")
 @RestController
 @RequiredArgsConstructor
 public class EtcApiController {
@@ -25,6 +29,7 @@ public class EtcApiController {
     /**
      * 공지사항 리스트 조회하기
      */
+    @Operation(summary = "공지사항 리스트 조회 API", description = "등록된 모든 공지사항의 리스트를 조회합니다.")
     @GetMapping("/notice")
     public ResponseEntity<Response<List<findAllNoticeResponse>>> readAllNotice() {
 
@@ -50,6 +55,8 @@ public class EtcApiController {
      * 고쳐야 할 부분
      * 1. noticeIndex 유효한지 확인.
      */
+    @Operation(summary = "공지사항 글 조회 API", description = "단일 공지사항 글을 조회합니다.")
+    @Parameter(name = "noticeIndex", description = "공지사항 id")
     @GetMapping("/notice/{noticeIndex}")
     public ResponseEntity<Response<Notice>> readANotice(
             @PathVariable("noticeIndex") Long noticeIndex) {
@@ -65,6 +72,7 @@ public class EtcApiController {
     /**
      * 이용약관 리스트 받기
      */
+    @Operation(summary = "이용약관 리스트 조회 API", description = "회원가입을 할 때, 이용약관 리스트를 보여주기 위해 사용합니다.")
     @GetMapping("/terms-of-service")
     public ResponseEntity<Response<List<TermsOfService>>> readAllTerms() {
 
