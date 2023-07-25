819397f9c79c518190ac8958d74ebc51212fa202
sp.arc <pcpark98@naver.com>
2023-07-26 01:58:13 +0900

    fix: 존재하지 않는 것에 대한 조회의 예외처리 완료

--- src/main/java/com/matdori/matdori/controller/JokboApiController.java
+++ src/main/java/com/matdori/matdori/controller/JokboApiController.java
@@ -53,10 +53,9 @@ public class JokboApiController {
      * 고쳐야 할 부분
      * 1. 세션 체크하고 시작하기.
      * 2. storeIndex 유효한지 확인.
-     * 3. 프론트랑 연결할 때, request에 @RequestBody 붙이기.
-     * 4. 족보 테이블에 넣고 이미지를 넣을 차례에 이미지를 넣다가 에러가 발생한 경우, 족보 테이블 롤백이 필요하다.
-     * 5. S3에 이미지 넣고, 족보 이미지 테이블에 넣을 차례에 에러가 발생하면, 족보 이미지 테이블에 롤백이 필요하다.
-     * 6. 족보 테이블에 저장, S3에 이미지 저장, 족보 이미지 테이블에 저장 -> 이 세가지를 한 트랜젝션에 묶어라.
+     * 3. 족보 테이블에 넣고 이미지를 넣을 차례에 이미지를 넣다가 에러가 발생한 경우, 족보 테이블 롤백이 필요하다.
+     * 4. S3에 이미지 넣고, 족보 이미지 테이블에 넣을 차례에 에러가 발생하면, 족보 이미지 테이블에 롤백이 필요하다.
+     * 5. 족보 테이블에 저장, S3에 이미지 저장, 족보 이미지 테이블에 저장 -> 이 세가지를 한 트랜젝션에 묶어라.
      */
     @Operation(summary = "족보 작성하기 API", description = "족보를 작성합니다.")
     @ApiResponses({
@@ -114,8 +113,7 @@ public class JokboApiController {
      * 족보 내용 조회하기
      *
      * 고쳐야 할 부분
-     * 1. 족보 id 유효한지 확인
-     * 2. 이미지 url들 조회할 때, service 한 번 더 호출하지 말고, jokbo.get 해서 가져오기.
+     * 1. 이미지 url들 조회할 때, service 한 번 더 호출하지 말고, jokbo.get 해서 가져오기.
      */
     @Operation(summary = "족보 내용 조회하기", description = "단일 족보의 상세 내용을 조회합니다.")
     @Parameter(name = "jokboIndex", description = "족보 id")
@@ -150,9 +148,8 @@ public class JokboApiController {
      * 내가 쓴 족보 삭제하기
      *
      * 고쳐야 할 부분
-     * 1. jokboId가 유효한지 확인
-     * 2. imgUrls 가져올 때, jokboService 호출하지 말고, jokboImgs.get(index).~ 로 가져오기.
-     * 3. 족보를 삭제한 이후에, S3에 저장된 이미지를 삭제하려다가 오류가 발생하면 롤백이 필요함. -> 한 트랜젝션으로 묶기
+     * 1. imgUrls 가져올 때, jokboService 호출하지 말고, jokboImgs.get(index).~ 로 가져오기.
+     * 2. 족보를 삭제한 이후에, S3에 저장된 이미지를 삭제하려다가 오류가 발생하면 롤백이 필요함. -> 한 트랜젝션으로 묶기
      */
     @Operation(summary = "내가 쓴 족보 삭제 API", description = "족보 게시글을 삭제합니다.")
     @Parameters({
@@ -192,9 +189,6 @@ public class JokboApiController {
 
     /**
      * 족보에 댓글 등록하기.
-     *
-     * 고쳐야 할 부분
-     * 1. jokboId가 유효한지 확인.
      */
     @Operation(summary = "족보 댓글 작성 API", description = "족보에 댓글을 작성합니다.")
     @Parameters({
@@ -240,7 +234,6 @@ public class JokboApiController {
      * 고쳐야 할 부분
      * 1. 페이징 처리
      * 2. 정렬 처리 구현 필요
-     * 3. jokboId가 존재하는 족보의 id인지 유효성 체크 필요.
      */
     @Operation(summary = "족보에 달린 모든 댓글 조회 API", description = "족보 게시글에 달린 모든 댓글들을 조회합니다.")
     @Parameter(name = "jokboIndex", description = "족보 id")
@@ -276,9 +269,6 @@ public class JokboApiController {
 
     /**
      * 내가 쓴 댓글 삭제하기.
-     *
-     * 고쳐야 할 부분
-     * 1. commentId가 존재하는 족보 댓글을 가리키는지 확인 필요.
      */
     @Operation(summary = "내가 쓴 댓글 삭제 API", description = "유저 본인이 작성한 댓글을 삭제합니다.")
     @Parameters({
@@ -331,7 +321,7 @@ public class JokboApiController {
      * 학과별 추천 식당 조회하기
      *
      * 고쳐야 할 부분
-     * 1. department가 유효한지 확인하기.
+     * 1. department가 유효한지 확인하기. -> 논의 필요. 유효한 department인지 확인하려면 모든 department를 DB에 따로 저장해야 하지 않을까?
      */
     @Operation(summary = "학과별 추천 식당 조회 API", description = "유저가 소속된 학과의 학생들이 족보를 많이 작성한 식당들을 조회합니다.")
     @Parameter(name = "department", description = "학과")
@@ -365,7 +355,7 @@ public class JokboApiController {
      * 맛도리 픽 가게 리스트 조회하기.
      *
      * 고쳐야 할 부분
-     * 1. department가 유효한지 확인.
+     * 1. department가 유효한지 확인. -> 논의 필요.
      */
     @Operation(summary = "맛도리 픽 가게 리스트 조회 API", description = "맛도리 픽이라는 이름으로 학과별 추천으로 선정되지 않은 가게들 중에서 랜덤으로 세 곳을 조회합니다.")
     @Parameter(name = "department", description = "학과")
