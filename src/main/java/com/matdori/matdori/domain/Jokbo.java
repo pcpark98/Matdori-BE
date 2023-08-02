9bfa64d6bd2993d98216d441063659f86da27a0c
sp.arc <pcpark98@naver.com>
2023-08-02 15:02:35 +0900

    feature: Jokbo 도메인 not null 처리 및 DB 타입에 맞게 수정

--- src/main/java/com/matdori/matdori/domain/Jokbo.java
+++ src/main/java/com/matdori/matdori/domain/Jokbo.java
@@ -6,6 +6,8 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.springframework.data.annotation.CreatedDate;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -28,7 +30,13 @@ public class Jokbo {
     @JoinColumn(name = "store_index")
     private Store store;
 
+    @NotBlank
+    @Column(length = 50)
+    @Size(max = 50)
     private String title;
+
+    @NotBlank
+    @Column(columnDefinition = "TEXT")
     private String contents;
 
     @Column(name = "flavor_rating")
