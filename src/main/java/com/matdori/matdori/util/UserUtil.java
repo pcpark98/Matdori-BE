8b10a77c1bf7300acf0f7303a6c2532e93621faa
StrangeMin <mim0107@nate.com>
2023-08-05 15:22:05 +0900

    fix : 아이디 정규표현식 수정

--- src/main/java/com/matdori/matdori/util/UserUtil.java
+++ src/main/java/com/matdori/matdori/util/UserUtil.java
@@ -7,7 +7,7 @@ import java.util.UUID;
 @UtilityClass
 public class UserUtil {
 
-    public static boolean isValidEmailFormat(String email) { return email.matches("[0-9]{8}@((inha.ac.kr)|(inha.edu))"); }
+    public static boolean isValidEmailFormat(String email) { return email.matches("[A-Za-z0-9]+@((inha.ac.kr)|(inha.edu))"); }
     public static boolean isValidPasswordFormat(String password) { return password.matches("^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,16}$"); }
 
     public static String getRandomNickname() { return UUID.randomUUID().toString(); }// 형용사 붙인 맛도리로..
