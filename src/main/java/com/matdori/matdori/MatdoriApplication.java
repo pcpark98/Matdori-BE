2057d3d2b245bd053afa5e9b297a57189b3c22a2
sp.arc <pcpark98@naver.com>
2023-08-08 17:11:56 +0900

    fix: 스웨거 https 적용

--- src/main/java/com/matdori/matdori/MatdoriApplication.java
+++ src/main/java/com/matdori/matdori/MatdoriApplication.java
@@ -1,8 +1,11 @@
 package com.matdori.matdori;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = {@Server(url = "https://api.matdori.app", description = "서버 URL")})
 @SpringBootApplication
 public class MatdoriApplication {
 
