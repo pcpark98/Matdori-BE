28d102941c087c2db812a9cbef58295f14593d8a
StrangeMin <mim0107@nate.com>
2023-08-17 14:21:17 +0900

    feat : 랜덤 메뉴 추천받기 API 생성

--- src/main/java/com/matdori/matdori/repositoy/Dto/RecommendedMenu.java
+++ src/main/java/com/matdori/matdori/repositoy/Dto/RecommendedMenu.java
@@ -5,24 +5,28 @@ import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public class RecommendedStore {
-    public RecommendedStore(Long storeId, String name, String imgUrl, Double flavorRating, Double underPricedRating, Double cleanRating) {
+public class RecommendedMenu {
+
+    private Long storeId;
+    private String storeName;
+    private String menuName;
+    private Double totalRating;
+    private String imgUrl;
+
+    public RecommendedMenu(Long storeId, String storeName, String menuName, String imgUrl, Double flavorRating, Double underPricedRating, Double cleanRating) {
         this.storeId = storeId;
-        this.name = name;
-        this.imgUrl = imgUrl;
+        this.storeName = storeName;
+        this.menuName = menuName;
         this.totalRating = getScore(flavorRating) + getScore(underPricedRating) + getScore(cleanRating);
         if(totalRating != 0.0) this.totalRating = Math.ceil(totalRating/3 * 10)/ 10;
         else this.totalRating = 0.0;
-
+        this.imgUrl = imgUrl;
     }
     public Double getScore(Double score) {
         if(score == null) return 0.0;
         else return score;
     }
-
-
-    private Long storeId;
-    private String name;
-    private String imgUrl;
-    private Double totalRating;
 }
+
+
+
