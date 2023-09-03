3983b34d93f981931c971de0ec69881deedda033
sp.arc <pcpark98@naver.com>
2023-09-03 16:48:36 +0900

    fix: Store 도메인의 category 길이 설정 제거

--- src/main/java/com/matdori/matdori/domain/Store.java
+++ src/main/java/com/matdori/matdori/domain/Store.java
@@ -24,12 +24,8 @@ public class Store {
     @Size(max = 30)
     private String name;
 
-    //@NotBlank
-    //@Column(length =20)
-    //@Size(max = 20)
     @Enumerated(EnumType.STRING)
     private StoreCategory category;
-
 
     @Column(name = "phone_number", length = 15)
     private String phoneNumber;
